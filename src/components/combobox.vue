de9e0a7214acc948fe5d95b0143348f3e8e6a144
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2022-05-15 16:09:27 +0200

    début projet

--- src/components/combobox.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <select class="bg-Gris/foncé text-white" name="test" id="select">
-      <option value="">Choisir une option</option>
-      <option value="">Choix 1</option>
-      <option value="">Choix 2</option>
-    </select>
-  </div>
-</template>
--- src/components/couleur.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="mx-4 h-32 w-32 text-Gris/foncé rounded-lg"><slots /></div>
-</template>
