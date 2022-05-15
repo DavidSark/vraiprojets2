b80a769e901a6a86cc2014a4d612c8bc880fd24a
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2022-05-15 16:51:05 +0200

    routes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<template>
+
+
+<template clas>
   <div class="bg-gradient-to-b from-baby-blue to-blue-electric h-full w-full">
-    <h1 class="font-quicksand text-baby-blue">Blumee</h1>
+    <h1 class="font-quicksand text-baby-blue text-center">Blumee</h1>
   </div>
+  <nav>
+    <RouterLink to="/home">HomeView</RouterLink>
+    <RouterLink/>
+  </nav>
 </template>
 
 
